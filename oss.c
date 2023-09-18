3569420f12bf5ef3ab7ab6ec2c3c75383d9dab5a
Erosh Boralugodage <erosh.bc@gmail.com>
2023-09-18 15:14:02 -0500

    create share mem key, shmget

--- oss.c
+++ oss.c
@@ -7,14 +7,32 @@
 #include <getopt.h>
 
 #create  processtable for shared memory
+struct PCB{
+
+}
+
 
 int main(int argc, char **argv){
     
     //generate unique key - ftok
+    key_t key_clock = {"key_clock", 4760};
 
     //returns an identifier in shmid- shmget
-
+    int shm_clock = shmget{key_clock, sizeof(PCB),0666|IPC_CREAT}
+    if(shm_clock == -1){
+        perror("shmget for clock");
+        return EXIT_FAILURE;
+    }
     //Attch to the shared memory for the simulated system clock
+    
+    //
+    
+    
+    
+    //deattach the shared memeory
+
+    //free the memory 
+    
     return 0;
 
 
