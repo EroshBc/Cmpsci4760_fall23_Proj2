8170a6df8707208bf5b021ab91333df7e6d309e2
Erosh Boralugodage <erosh.bc@gmail.com>
2023-09-18 02:26:07 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Project 2
+Erosh Boralugodage
+CMPSCI4760 - Fall 2023
+
+** operatin system **
---
+++ oss.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+
+
+int main(int argc, char **argv){
+    printf("Now in oss\n");
+
+    return 0;
+
+
+}
