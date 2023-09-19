aec1f204f2f5b93be2e64af32e384e6599e46298
Erosh Boralugodage <erosh.bc@gmail.com>
2023-09-18 23:07:33 -0500

    complete share memory

--- oss.c
+++ oss.c
@@ -6,24 +6,30 @@
 #include <stdlib.h>
 #include <getopt.h>
 
-#create  processtable for shared memory
-struct PCB{
+//create  processtable for shared memory
+typedef struct {
 
-}
+}PCB;
 
 
 int main(int argc, char **argv){
     
     //generate unique key - ftok
-    key_t key_clock = {"key_clock", 4760};
+    key_t key_clock = ftok("key_clock", 4760);
 
     //returns an identifier in shmid- shmget
-    int shm_clock = shmget{key_clock, sizeof(PCB),0666|IPC_CREAT}
+    int shm_clock = shmget(key_clock, sizeof(PCB),0666|IPC_CREAT);
     if(shm_clock == -1){
         perror("shmget for clock");
         return EXIT_FAILURE;
     }
     //Attch to the shared memory for the simulated system clock
+    PCB *clockData = (PCB *)shmat(shm_clock,NULL, 0);
+
+    if((void *) clockData == (void *) -1){
+        perror("shmat for clock");
+        return EXIT_FAILURE;
+    }
     
     //
     
