9e73100edf4f9144fccd04153da9e89f62e06f41
Erosh Boralugodage <erosh.bc@gmail.com>
2023-09-20 00:00:47 -0500

    sec and nano sec passd to worker

--- oss.c
+++ oss.c
@@ -25,7 +25,7 @@ int main(int argc, char **argv){
     int n=0;
     int s=0;
     int t=0;
-
+    
 	while((opt_val = getopt(argc, argv, "hn:s:t:")) != -1){ // getopt() function take arguments -n, -s ,-t
 		switch (opt_val){
 	
@@ -36,7 +36,7 @@ int main(int argc, char **argv){
             printf("simul- how many children to run simulataneously.\n");
             printf("iter -- time that a child process will be launched\n");
             printf("\n\n");
-			break;
+			break; 
 		case 'n':
             n = atoi(optarg);
 			break;
@@ -44,7 +44,7 @@ int main(int argc, char **argv){
 		case 's':
 			s = atoi(optarg);
 			break;
-
+    
         case 't':
 			t = atoi(optarg);
 			break;
@@ -85,10 +85,17 @@ int main(int argc, char **argv){
     //initailize sec amd nanoSec and set to zero in shared memory
     clockData->sec = 0;
     clockData->nanoSec = 0;
+
     
+    
+
 
     // get random number between 1 and t for seconds and nano seconds to pass to worker
     srand(time(0));
+    
+    int sec_worker = (rand()%(t-1)) + 1;
+    int nanoSec_worker = (rand()%1000000000) +1;
+   
 
 
 
@@ -97,25 +104,27 @@ int main(int argc, char **argv){
 
     
 
-    /*
     
+  /*  
    int simul_counter =0;
    
    
     for(int tot_chldrn=0; tot_chldrn<n;){
         
-        if(simul_counter < s){
+       if(simul_counter < s){
             
-            printf("simul is %d and children %d\n\n",simul_counter+1, tot_chldrn+1);
+            printf("simul is %d and children %d\n\n",simul_counter+1, tot_chldrn+1);*/
             
             pid_t childPid = fork(); // This is where the child process splits from the parent
             
 
             if(childPid == 0){ //child process call worker "t" times
-                char args[2];
-                sprintf(args,"%d",t);
+                char args_sec[2];
+                char args_nanosec[3];
+                sprintf(args_sec,"%d",sec_worker );
+                sprintf(args_nanosec,"%d",nanoSec_worker );
                 
-                execl("./worker","worker",args,NULL);
+                execl("./worker","worker",args_sec,args_nanosec,NULL);
 
                 perror("exec failed\n");
                 exit(1);
@@ -123,11 +132,11 @@ int main(int argc, char **argv){
 
                 
             }else if(childPid > 0){
-
+                
               wait(0);
 
-              simul_counter += 1;
-              tot_chldrn += 1;
+            //  simul_counter += 1;
+            //  tot_chldrn += 1;
 
               
                
@@ -137,28 +146,29 @@ int main(int argc, char **argv){
                 exit(1);
             }
 
-        }else{
-
+        //}
+        /*else{
+           
             //wait for child to finish
             simul_counter = 1;
             printf("\n***child to finish***\n");
             //printf("simul is %d and children %d\n\n",simul_counter+1, tot_chldrn+1);
             wait(0);
-            simul_counter -= 1;
-
+            simul_counter -= 1;     */
             
-        }  
+     //   }  
      
     
-    }
+   // }
+  
         //wait for all remaining child process to finish
-        while(simul_counter > 0){
+      /*  while(simul_counter > 0){
             
             wait(0);
             simul_counter -=1;
             
-        }
+        }*/
     
-*/
+
     return 0;
 }
--- worker.c
+++ worker.c
@@ -4,19 +4,26 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]){
-    //take not more than one input if not error message
+    //take not more than two input if not error message
+    printf("Now in worker\n");
     
-    int iter =0;
+    //sec and nano secs form oss files and 
+    int sec_from_oss = 0;
+    int nanosec_from_oss = 0;
     
-    if (argc != 2){
-        fprintf(stderr,"Usage: %s <iterations>\n",argv[0]);
+
+    sec_from_oss = atoi(argv[1]);
+    nanosec_from_oss = atoi(argv[2]);
+    printf("%d   %d\n",sec_from_oss, nanosec_from_oss);
+
+    if (argc != 3){
+        fprintf(stderr,"Usage: %s seconds and nanoseconds\n",argv[0]);
         exit(1);
     }
 
-    iter = atoi(argv[1]);
     
-    
-    
+  
+   /* 
     pid_t pid = getpid();
     pid_t ppid = getppid();
 
@@ -27,6 +34,7 @@ int main(int argc, char *argv[]){
 
     printf("worker PID:%d PPID:%d iterations:%d after sleeping\n",pid, ppid, i+1);
     }
+    */
     
     return EXIT_SUCCESS;
 } 
